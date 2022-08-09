c66435a4689b83c1390170b3421ae8012f5ba58e
lee-ji-hong <vhfhfldi0303@hotelga.co.kr>
2022-08-09 14:01:23 +0900

    Feat: "대문자로 변환"

---
+++ Inflean/section1/13.대문자로 변환/index.js
@@ -0,0 +1,13 @@
+function solution(str) {
+  let answer = '';
+  for ( let x of str) {
+    if( x == x.toLowerCase() ){
+      answer += x.toUpperCase();
+    }else 
+    answer += x.toLowerCase();
+  }
+  return answer;
+}
+
+let str = 'StuDY';
+console.log(solution(str));
