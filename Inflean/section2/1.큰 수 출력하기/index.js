7f9b3f7a331138508a16c6ccb72c7257033528a6
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2022-10-24 15:30:51 +0900

    Feat: "큰 수 출력하기"

--- Inflean/section2/1.큰 수 출력하기/index.js
+++ Inflean/section2/1.큰 수 출력하기/index.js
@@ -1,6 +1,6 @@
 function solution(arr) {
   let answer = [];
-  answer.push(arr[0]);
+  answer.push(arr[0]); //첫번째 수는 무조건 출력하라는 조건을 충족시키기 위해
   for( let i = 1; i < arr.length; i++ ){
     if(arr[i]>arr[i-1]){
       answer.push(arr[i])
