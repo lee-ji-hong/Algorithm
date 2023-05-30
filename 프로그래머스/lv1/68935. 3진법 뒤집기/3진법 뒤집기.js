e4d50a0c7e032b278710fe032d71769dce8e44b5
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2023-05-30 14:47:50 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,9 +1,3 @@
 function solution(n) {
-    let ternary = '';
-    
-    while(n > 0) {
-        ternary +=(n % 3);
-        n = Math.floor(n/3);
-    }
-    return parseInt(ternary,3);
+    return parseInt(n.toString(3).split('').reverse().join(''), 3);
 }
