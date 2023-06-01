ef50c529637b786c30765fff8b5d88d21377d388
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2023-06-01 13:14:26 +0900

    [unrated] Title: 과일 장수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,24 +1,8 @@
-function chunk(data = [], size = 1) {
-  const arr = [];
-    
-  for (let i = 0; i < data.length; i += size) {
-    arr.push(data.slice(i, i + size));
-  }
-
-  return arr;
-}
-
 function solution(k, m, score) {
-    score.sort((a,b) => b-a)
-    var answer = 0;
-    const array = chunk(score,m);
-    
-    for(let i = 0; i<array.length; i++){
-        const min = Math.min.apply(null, array[i]);
-        if(array[i].length == m ) {
-            answer += (min * m);
-        }
-        answer+= 0;
+    let answer = 0;
+    const sortedScore = score.slice().sort((a, b) => a - b).slice(score.length % m);
+    for (let i = 0; i < sortedScore.length; i += m) {
+        answer += sortedScore[i] * m;
     }
     return answer;
 }
