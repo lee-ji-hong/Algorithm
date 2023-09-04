599af20e21ed2053c4478ff080d571699c50809e
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2023-09-04 22:13:10 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    const dp = new Array(n).fill(0);
+    dp[0] = 1;
+    dp[1] = 2;
+    for(let i = 2; i < n; i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    }
+    return dp[n-1];
+}
