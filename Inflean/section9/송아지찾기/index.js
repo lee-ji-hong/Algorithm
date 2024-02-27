42c4fc8598284aca50a2427d34faacf958435ca6
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-02-27 18:14:52 +0900

    Feat:"송아지찾기"

--- Inflean/section9/송아지찾기/index.js
+++ Inflean/section9/송아지찾기/index.js
@@ -1,5 +1,24 @@
 function solution(s, e) {
-    
+    let answer = 0;
+    let ch = Array.from({ length: 10001 }, () => 0); //boolean
+    let dis = Array.from({ length: 10001 }, () => 0); //레벨
+    let queue = [];
+    queue.push(s);
+    ch[s] = 1;
+    dis[s] = 0;
+
+    while(queue.push){
+        let x = queue.shift();
+        for(let nx of [x+1,x-1,x+5]){
+            if(nx===e) return dis[x]+1;
+            if(ch[nx]===0 && nx>0 &&nx<10001){
+                ch[nx]=1
+                queue.push(nx)
+                dis[nx] = dis[x]+1 //x레벨보다 1레벨 높으니까 1레벨씩 +하는거
+            }
+        }
+    }
+
     return answer;
 }
 
