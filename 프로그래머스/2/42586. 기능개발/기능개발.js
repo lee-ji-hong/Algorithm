9a94a0b2b6967c6cf2c54e075deeead232330dde
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2024-06-09 22:07:43 +0900

    [level 2] Title: 기능개발, Time: 3.12 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,16 +1,29 @@
 function solution(progresses, speeds) {
     let answer = [0];
-    let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
-    let maxDay = days[0];
+    let term =[];
+    let maxDay=[];
 
-    for(let i = 0, j = 0; i< days.length; i++){
-        if(days[i] <= maxDay) {
-            answer[j] += 1;
-        } else {
-            maxDay = days[i];
-            answer[++j] = 1;
+    for(let i=0; i< progresses.length; i++){
+        let test = Math.ceil((100-progresses[i])/speeds[i]);
+        term.push(test)
+    }
+    
+    maxDay=term[0];
+    
+    for(let m=0,n=0; m<term.length; m++){
+        if(term[m]<=maxDay){
+            answer[n]+=1;
+        }else{
+            maxDay = term[m];
+            answer[++n]=1;
         }
     }
-
+    console.log(term)
+    
     return answer;
 }
+
+//작업의 진도 = [progresses]
+//개발 속도 = [speeds]
+//배포는 순서대로 진행되어야 한다. 
+//각 배포마다 몇개의 기능이 배포되는지 return
