d3f1b6a7ac0d4883ecd4b0715c4589b4d1fb2c10
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2025-11-01 17:12:13 +0900

    [level 1] Title: 공원 산책, Time: 0.34 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -5,37 +5,32 @@
 // 공원을 나타내는 배열 park, 로봇 강아지가 수행할 배열 routes
 // 최종 강아지의 위치 좌표는?
 function solution(park, routes) {
-  const H = park.length;
-  const W = park[0].length;
+    const H = park.length, W = park[0].length;
+    const DIR = { N:[-1,0],S:[1,0],W:[0,-1],E:[0,1]};
 
-  // 시작점 S 찾기
-  let sr = 0, sc = 0;
-  for (let r = 0; r < H; r++) {
-    const c = park[r].indexOf('S');
-    if (c !== -1) { sr = r; sc = c; break; }
-  }
-
-  // 방향 벡터
-  const DIR = { 'N': [-1, 0], 'S': [1, 0], 'W': [0, -1], 'E': [0, 1] };
-
-  // 명령 처리
-  for (const cmd of routes) {
-    const [d, stepStr] = cmd.split(' ');
-    const step = Number(stepStr);
-    const [dr, dc] = DIR[d];
-
-    let nr = sr, nc = sc;
-    let ok = true;
-
-    // 한 칸씩 검증 이동
-    for (let k = 0; k < step; k++) {
-      nr += dr; nc += dc;
-      if (nr < 0 || nr >= H || nc < 0 || nc >= W || park[nr][nc] === 'X') {
-        ok = false; break;
-      }
+    // 시작점 S 구하는 코드
+    let sr = 0, sc = 0; 
+    for(let r =0; r<H; r++){
+        const c = park[r].indexOf('S');
+        if(c !== -1){ sr = r; sc = c; break; }
     }
-    if (ok) { sr = nr; sc = nc; } // 전부 가능하면 위치 확정
-  }
 
-  return [sr, sc];
+    for (const cmd of routes) {
+        const [d, raw] = cmd.split(' ');
+        const step = Number(raw);
+        const [dr, dc] = DIR[d];
+        
+        let nr =sr, nc = sc; // 명령 수행 후 이동한 위치
+        let visited = true;
+        
+        for(let k =0; k<step; k++){
+            nr+=dr; nc+=dc;
+            if(nr<0 || nr>=H || nc <0 || nc>=W || park[nr][nc] === 'X'){
+                visited = false; break;
+            }
+        }
+        if(visited){ sr=nr; sc=nc;}
+    }
+    
+    return [sr,sc];
 }
