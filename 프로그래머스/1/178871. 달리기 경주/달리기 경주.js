81824c4bea03e3535c99267ff2ea6d6e697988f2
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2025-11-03 19:36:18 +0900

    [level 1] Title: 달리기 경주, Time: 245.21 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,25 +1,50 @@
 // 현재 순위를 Map으로 저장해두고 swap할때 배열과 Map을 동시에 업데이트하면 각 호출을 O(1)에 처리 
+// function solution(players, callings) {
+//   const answer = players.slice();           // 원본 보존
+//   const pos = new Map();                    // 이름 -> 현재 인덱스
+
+//   for (let i = 0; i < answer.length; i++) {
+//     pos.set(answer[i], i);
+//   }
+
+//   for (const name of callings) {
+//     const i = pos.get(name);
+//     if (i === 0) continue;// 이미 선두면 패스
+
+//     const prevName = answer[i - 1];
+
+//     // 배열에서 자리 바꾸기
+//     [answer[i - 1], answer[i]] = [answer[i], answer[i - 1]];
+
+//     // 위치 Map 동기화
+//     pos.set(name, i - 1);
+//     pos.set(prevName, i);
+//   }
+
+//   return answer;
+// }
+
+//  players : 현재 등수
+//  callings : 부른 배열
+//  return 최종 순위
 function solution(players, callings) {
-  const answer = players.slice();           // 원본 보존
-  const pos = new Map();                    // 이름 -> 현재 인덱스
-
-  for (let i = 0; i < answer.length; i++) {
-    pos.set(answer[i], i);
+    const array = players.slice();
+    const map = new Map();
+    
+  for (let i = 0; i < array.length; i++) {
+    map.set(array[i], i);
   }
-
-  for (const name of callings) {
-    const i = pos.get(name);
-    if (i === 0) continue;                  // 이미 선두면 패스
-
-    const prevName = answer[i - 1];
-
-    // 배열에서 자리 바꾸기
-    [answer[i - 1], answer[i]] = [answer[i], answer[i - 1]];
-
-    // 위치 Map 동기화
-    pos.set(name, i - 1);
-    pos.set(prevName, i);
-  }
-
-  return answer;
+    // 호출 배열 반복, 선두일 경우 continue,그렇지 않을 경우
+    for(const member of callings){
+        const i = map.get(member);
+        if(i === 0 ) continue;
+        const preName = array[i-1];
+        
+        [array[i - 1], array[i]] = [array[i], array[i - 1]];
+        map.set(member,i-1);
+        map.set(preName,i);
+    }
+    
+    return array
+    
 }
