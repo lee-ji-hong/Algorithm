953e8cf133cfe716e5364e87d52319cdbc1ef14e
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2025-11-03 23:34:34 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 26.70 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,28 +1,32 @@
 function solution(queue1, queue2) {
-    const cq = queue1.concat(queue2);
-    const n = cq.length;
-    let answer = 0;
-    let sum1 = queue1.reduce((acc, cur) => acc + cur, 0,);
-    let sum2 = queue2.reduce((acc, cur) => acc + cur, 0,);
-    let p1 = 0;
-    let p2 = Math.floor(n / 2);
+  const n1 = queue1.length, n2 = queue2.length;
+  const N = n1 + n2;
 
-    while (p1 < n & p2 < n) {
-        if (sum1 === sum2) {
-            return answer
-        }
-        else if (sum1 > sum2) {
-            const tmp = cq[p1++];
-            sum1 -= tmp;
-            sum2 += tmp;
-        }
-        else if (sum1 < sum2) {
-            const tmp = cq[p2++];
-            sum1 += tmp;
-            sum2 -= tmp;
-        }
-        answer++;
+  const sum = a => a.reduce((x, y) => x + y, 0);
+  let s1 = sum(queue1), s2 = sum(queue2);
+  const total = s1 + s2;
+  if (total % 2) return -1;
+  const target = total / 2;
+  if (s1 === target) return 0;
+
+  let h1 = 0, h2 = 0, moves = 0;
+  const LIMIT = 2 * N;           // 외우기 쉬운 상한
+
+  while (moves <= LIMIT) {
+    if (s1 === target) return moves;
+
+    if (s1 > target) {           // q1 -> q2
+      const x = queue1[h1++];
+      if (x === undefined) return -1;
+      s1 -= x; s2 += x;
+      queue2.push(x);
+    } else {                     // q2 -> q1
+      const x = queue2[h2++];
+      if (x === undefined) return -1;
+      s2 -= x; s1 += x;
+      queue1.push(x);
     }
-
-    return -1;
+    moves++;
+  }
+  return -1;                     // 상한 내에 못 맞추면 불가
 }
